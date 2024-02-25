d3177a0f9db756e31b45c7de3c2eb424818a4ffa
Aakash Nishad <111107009+aksxil@users.noreply.github.com>
2024-02-25 11:59:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,44 +1,61 @@
-require("dotenv").config({ path: "./.env" });
+require("dotenv").config({path: "./.env"})
 const express = require("express");
 const app = express();
-const cors = require("cors");
-const logger = require("morgan");
-const session = require("express-session");
-const cookieParser = require("cookie-parser");
-const fileUpload = require("express-fileupload");
-const ErrorHandler = require("./utils/ErrorHandler");
-const { generatedErrors } = require("./middlewares/errors");
-const { connectDatabase } = require("./models/database");
+const cors = require("cors")
 
-// Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
-app.use(logger("tiny"));
+
+
+//-------------------DEMO-------------------
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true,
+  };
+  
+  app.use(cors(corsOptions));
+  
+  
+//-------------------DEMO-------------------
+
+//db connection 
+require("./models/database").connectDatabase();
+
+//logger
+
+const logger = require("morgan");
+app.use(logger("tiny"))
+//bodyparser
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.urlencoded({extended: false}))
+
+//session and cookie
+
+const session = require("express-session");
+const cookieparser = require("cookie-parser")
 app.use(session({
     resave: true,
     saveUninitialized: true,
     secret: process.env.EXPRESS_SESSION_SECRET
-}));
-app.use(fileUpload());
+})) 
 
-// Database connection
-connectDatabase();
+app.use(cookieparser());
 
-// Routes
-app.use("/", require("./routes/indexRoutes"));
-app.use("/resume", require("./routes/resumeRoutes"));
-app.use("/employe", require("./routes/employeRoutes"));
+//express file-upload 
+const fileupload = require("express-fileupload");
+app.use(fileupload());
 
-// Error handling
-app.use((req, res, next) => {
-    next(new ErrorHandler(`URL is not found ${req.url}`, 404));
-});
-app.use(generatedErrors);
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+//routes
+app.use("/",require("./routes/indexRoutes"))
+app.use("/resume",require("./routes/resumeRoutes"))
+app.use("/employe",require("./routes/employeRoutes"))
+
+//error handling
+const ErrorHandler = require("./utils/ErrorHandler");
+const { generatedErrors } = require("./middlewares/errors");
+app.all("*",(req,res,next)=>{
+    next(new ErrorHandler(`url is not found ${req.url}`,404))
+})
+app.use(generatedErrors)
+
+
+app.listen(process.env.PORT, console.log(`SERVER IS RUNNING ON ${process.env.PORT}`))
